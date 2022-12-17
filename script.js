7ce00b5c4ea4201d13f18b1ea2d6297de2f7d1cd
jaheemprevost <jaheemdp@gmail.com>
2022-12-17 00:03:50 -0400

    Create a layout for large screens using desktop-design.jpg as reference and media queries

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const openMenuBtn = document.querySelector('.open-menu-btn');
 const closeMenuBtn = document.querySelector('.close-menu-btn');
 const overlay = document.querySelector('.overlay');
+const mainImage = document.querySelector('.main-image');
+const minWidth = 1200;
 
+/*If the device screen is larger than minWidth change the main image's 
+source to the desktop version of the article image.*/
+
+if (window.innerWidth >= minWidth) {
+  mainImage.setAttribute('src', './assets/images/image-web-3-desktop.jpg');
+}
 //Function that removes hidden from the list of overlay's classes.
 const openMenu = function() {
   overlay.classList.remove('hidden');
